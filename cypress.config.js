d3912b2f6935497e548880a876dbcad0205108ec
Marcus <marcusmvritec@gmail.com>
2023-07-22 17:46:38 -0300

    secao 8 - 28

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,11 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
+    env: {
+      viewportWidthBreakpoint: 768,
+    },    
     defaultCommandTimeout: 40000,
-    requestTimeout: 4000,
+    requestTimeout: 6000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
