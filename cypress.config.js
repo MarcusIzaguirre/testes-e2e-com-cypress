519c6343394b9e709f366bc7fa6954c92351d4a4
Marcus <marcusmvritec@gmail.com>
2023-07-21 21:57:12 -0300

    Ate aula 5

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
     defaultCommandTimeout: 40000,
+    requestTimeout: 4000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,6 +1,6 @@
 import 'cypress-mailosaur'
-
 import './commands'
+import 'cypress-iframe'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
