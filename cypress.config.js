210e670cc7ac8b16044c626a10ec067445e2199a
Marcus <marcusmvritec@gmail.com>
2023-07-27 17:19:22 -0300

    Final da aula 7

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,11 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
+    setupNodeEvents(on, config) {
+      require('@cypress/grep/src/plugin')(config)
+      return config
+    },
+    
   },
   projectId: 'wsy2aw'
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,3 +1,9 @@
+import registerCypressGrep from '@cypress/grep/src/support'
+registerCypressGrep()
+
+// Outros imports aqui.
+
+
 import 'cypress-mailosaur'
 import './commands'
 import 'cypress-iframe'
